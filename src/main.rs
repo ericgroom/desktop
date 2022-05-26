1430aa48ff222ad3e2372627b48e7efb1338107b
Eric Groom <bcgroom@gmail.com>
2022-05-26 08:53:43 -0700

    draw a circle

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use windows::{
 };
 
 use bevy::prelude::*;
+use bevy::sprite::MaterialMesh2dBundle;
 
 fn main() {
     unsafe {
@@ -18,11 +19,34 @@ fn main() {
         .add_plugin(bevy::hierarchy::HierarchyPlugin)
         .add_plugin(bevy::diagnostic::DiagnosticsPlugin)
         .add_plugin(bevy::asset::AssetPlugin)
-        .add_plugin(bevy::window::WindowPlugin { add_primary_window: false, exit_on_close: true })
+        .add_plugin(bevy::window::WindowPlugin { add_primary_window: true, exit_on_close: true })
+        .add_plugin(bevy::winit::WinitPlugin)
+        .add_plugin(bevy::input::InputPlugin)
         .add_plugin(bevy::render::RenderPlugin)
         .add_plugin(bevy::core_pipeline::CorePipelinePlugin)
         .add_plugin(bevy::pbr::PbrPlugin)
+        .add_plugin(bevy::sprite::SpritePlugin)
+        .add_startup_system(setup_camera)
+        .add_system(draw_circle)
+        .run()
         ;
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+}
+
+fn draw_circle(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>
+) {
+    commands.spawn_bundle(MaterialMesh2dBundle {
+        mesh: meshes.add(Mesh::from(shape::Quad::default())).into(),
+        transform: Transform::default().with_scale(Vec3::splat(128.)),
+        material: materials.add(ColorMaterial::from(Color::BLUE)),
+        ..default()
+    });
 }
 
 static mut WORKER_W: Option<HWND> = None;
